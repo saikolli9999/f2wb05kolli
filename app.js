1f58d18c66d2ab2bfa3b939807302e43636830a9
Sai Kumar Kolli <s548405@nwmissouri.edu>
2022-10-31 20:39:02 -0500

    engines class

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var engineRouter = require('./routes/engines');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/engines', engineRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
